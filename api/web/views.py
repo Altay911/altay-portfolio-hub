d8bb89772649d7722d0ecbcde9415c38c049ee48
Altay911 <altai.baghirov@gmail.com>
2026-04-17 15:51:00 +0200

    update some stuff

--- api/web/views.py
+++ api/web/views.py
@@ -13,4 +13,4 @@ def home_view(request):
 
 
 def resume_view(request):
-    return redirect('https://drive.google.com/file/d/1aDwx5j2SdcXPc4hGHjeZ9AFE79LZMyN7/preview')
+    return redirect('https://drive.google.com/file/d/12eQB5DD1SrZ2M_eO2P-UpCwYDwzu3IOB/preview')
